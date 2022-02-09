7781edf0f6bc4b444362be7efec0ca5f9700b4ae
aakash147 <akashrahim1122@gmail.com>
2022-02-09 17:02:03 +0500

    first commit

---
+++ src/components/Header/header.js
@@ -0,0 +1,12 @@
+
+import './header.css';
+
+export default () => {
+   return (
+      <nav>
+         <div className='container'>
+            <h2>Online Shoe Store</h2>
+         </div>
+      </nav>
+   );
+};
