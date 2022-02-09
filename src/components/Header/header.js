a7317405584bdf028917e5d27c57a05256a370e6
aakash147 <akashrahim1122@gmail.com>
2022-02-09 19:41:52 +0500

    changes in header file

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -6,6 +6,10 @@ export default () => {
       <nav>
          <div className='container'>
             <h2>Online Shoe Store</h2>
+            <i class="fas fa-shopping-cart">
+               <span className='cartCount'>0</span>
+            </i>
+
          </div>
       </nav>
    );
