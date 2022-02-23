6eb8c8dd1a422dad4a222e6f20982ade5bbee8e3
aakash147 <akashrahim1122@gmail.com>
2022-02-23 18:15:19 +0500

    Styling of product detail section

--- src/components/product/perProduct.js
+++ src/components/product/perProduct.js
@@ -14,9 +14,33 @@ export default ({ ads }) => {
 
     let { title, url, price } = shoe[0];
 
-    return <div>
-        <h1>{title}</h1>
-        <h3>{price}</h3>
-        <img src={url} />
+    return <div className="detailContainer">
+
+        <div className="detailSectionImage">
+            <img src={url} className='innerPic' />
+        </div>
+
+        <div className="descriptionOfItem">
+            <div className="itemShortDetail">
+                <h1 className="titleNameStyle">{title}</h1>
+                <p className="shortInfo">STYLE: Sport-Men-Shoes</p>
+                <p className="review">Be the first one to review</p>
+            </div>
+
+            <div className="priceBox">
+                <span className="price">{price}</span>
+            </div>
+
+            <div className="btnContainer">
+                <button className="cartButton">ADD TO CART</button>
+            </div>
+
+            <div className="shipmentBox">
+                <p className="shipmentInfo">
+                    Shipment will take 3-5 days after confirmation of the order.
+                </p>
+            </div>
+
+        </div>
     </div>
 }
